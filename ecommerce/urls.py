c5ae7757fb4286445bd2872d84ae3f686a06d455
unknown <knaub_elena@yahoo.com>
2021-12-22 13:20:33 +0200

    changed urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,3 @@ urlpatterns = [
     path('', include('store.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -36,10 +36,10 @@ def books(request):
           #parent is order and orderitem is child and we grab all children with _set.all
           items = order.orderitem_set.all()
           cartItems = order.get_cart_items
-     # else:
-     #      items = []
-     #      order = {'get_cart_total':0, 'get_cart_items':0}
-     #      cartItems = order['get_cart_items']
+     else:
+          items = []
+          order = {'get_cart_total':0, 'get_cart_items':0}
+          cartItems = order['get_cart_items']
 
      products = Product.objects.all()
      context = {'products':products, 'cartItems':cartItems}
